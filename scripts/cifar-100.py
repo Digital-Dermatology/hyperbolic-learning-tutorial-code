77f8d920bc4b008aac926740ce5bec448cf4d2fe
Simone Lionetti <simone.lionetti@hslu.ch>
2025-02-05 16:32:50 +0100

    Training on device

--- scripts/cifar-100.py
+++ scripts/cifar-100.py
@@ -2,27 +2,35 @@ import os
 
 import torch
 import torchvision
+from torch.optim import AdamW
+from torchinfo import summary
 from torchmetrics import MetricCollection
 from torchmetrics.classification import MulticlassAccuracy, MulticlassMatthewsCorrCoef
+from tqdm import tqdm
 
 from nets.euclidean import make_euclidean_convnet
+from utils.torch_utils import get_available_device, set_seeds
 
-num_workers = 0#os.cpu_count()
+num_workers = 0
 batch_size = 128
+num_epochs = 10
 
 
 def evaluate(
     loader: torch.utils.data.DataLoader,
     model: torch.nn.Module,
     metrics: MetricCollection,
+    device: torch.device,
 ):
+    metrics.reset()
     with torch.no_grad():
         for data, labels in loader:
-            outputs = model(data)
-            metrics(outputs, labels)
+            outputs = model(data.to(device))
+            metrics(outputs, labels.to(device))
 
 
 if __name__ == "__main__":
+    device = torch.device(get_available_device())
     transform = torchvision.transforms.Compose(
         [
             torchvision.transforms.ToTensor(),
@@ -51,7 +59,9 @@ if __name__ == "__main__":
     assert test_dataset.classes == classes
     num_classes = len(classes)
 
-    convnet = make_euclidean_convnet(out_channels=num_classes)
+    convnet = make_euclidean_convnet(out_channels=num_classes, conv_channels=(64, 64), fc_channels=(128,))
+    convnet.to(device)
+    summary(convnet)
 
     metrics = MetricCollection(
         [
@@ -59,15 +69,25 @@ if __name__ == "__main__":
             MulticlassMatthewsCorrCoef(num_classes=num_classes),
         ]
     )
+    metrics.to(device)
 
-    evaluate(loader=test_dataloader, model=convnet, metrics=metrics)
+    evaluate(loader=test_dataloader, model=convnet, metrics=metrics, device=device)
+    print("Metrics before training:")
     print(metrics.compute())
+    metrics.reset()
 
-    # criterion = torch.nn.CrossEntropyLoss()
-    # from torch.optim import AdamW
-    #
-    # optimizer = AdamW(convnet.parameters())
-    #
-    # for x, y in train_dataset:
-    #     print(x.min(), x.max())
-    #     break
+    criterion = torch.nn.CrossEntropyLoss()
+    criterion.to(device)
+    optimizer = AdamW(convnet.parameters())
+
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch + 1} of {num_epochs}")
+        for data, labels in tqdm(train_dataloader):
+            optimizer.zero_grad()
+            outputs = convnet(data.to(device))
+            loss = criterion(outputs, labels.to(device))
+            loss.backward()
+            optimizer.step()
+        evaluate(loader=test_dataloader, model=convnet, metrics=metrics, device=device)
+        print(metrics.compute())
+        metrics.reset()
