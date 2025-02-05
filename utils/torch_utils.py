5a79a29058d4489ebcfb9df0f48913bac492a6e3
Simone Lionetti <simone.lionetti@hslu.ch>
2025-02-05 08:16:04 +0100

    Add utils

---
+++ utils/torch_utils.py
@@ -0,0 +1,28 @@
+import os
+import random
+from pathlib import Path
+
+import numpy as np
+import torch
+
+
+def set_seeds(seed: int = 42) -> None:
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+
+def get_library_root() -> Path:
+    return Path(__file__).resolve().parent.parent.parent
+
+
+def get_available_device() -> str:
+    if torch.cuda.is_available():
+        return "cuda"
+    if torch.backends.mps.is_available():
+        return "mps"
+    return "cpu"
